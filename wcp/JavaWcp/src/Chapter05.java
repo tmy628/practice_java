a469981d215739240fee3e1254b91ef7361c8bbd
tmy628 <tmy6h1r@gmail.com>
2021-07-21 15:41:32 +0900

    [add] Chapter05

---
+++ wcp/JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 9223372036854775807L;
+		System.out.println(l);
+		double d = 1234567.89;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 0.123f;
+		System.out.println(f);
+		Integer i = 23;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(array[3]);
+	}
+}
