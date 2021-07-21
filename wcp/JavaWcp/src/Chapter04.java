d5c43e9bbb5a8b1acacc13d142362f6523def33a
tmy628 <tmy6h1r@gmail.com>
2021-07-21 13:39:45 +0900

    [add] Chapter04

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "tomoya";
+		System.out.println(str);
+		str = "艱難汝を玉にす";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
